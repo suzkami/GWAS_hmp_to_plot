6153fb52f68dbe6ed1c10aef82b82cb46fd442d6
suzkami <49057273+suzkami@users.noreply.github.com>
2023-07-16 17:13:22 +0900

    Update GWAS_RtoP_su.R

--- GWAS_RtoP_su.R
+++ GWAS_RtoP_su.R
@@ -178,14 +178,24 @@ MTA_range <- function(sigsnp_hmp, LDG){
     }else if (snp_chr$pos[nrow(snp_chr)] - snp_chr$pos[1] < LDG*1000000){
       assign(paste0(traitname, chr[i], "MTA_", index1), snp_chr)
     }else{
-      for (n in 2:nrow(snp_chr)) {
-        pos_dis <- snp_chr$pos[n] - snp_chr$pos[n-1]
-        if (pos_dis > LDG*1000000) {
-          assign(paste0(traitname, chr[i], "MTA_", index1), snp_chr[(n-index2):n,])
-          index1 <- index1 + 1
-          index2 <- 1
+      for (n in 2:(nrow(snp_chr))+1){
+        if (n <= nrow(snp_chr)) {
+        
+          pos_dis <- snp_chr$pos[n] - snp_chr$pos[n-1]
+          if (pos_dis > LDG*1000000) {
+            assign(paste0(traitname, chr[i], "MTA_", index1), snp_chr[(n-index2):(n-1)])
+            index1 <- index1 + 1
+            index2 <- 1
+          }else{
+            index2 <- index2 + 1 
+          }
+          
         }else{
-          index2 <- index2 + 1 
+          pos_dis <- snp_chr$pos[n-1] - snp_chr$pos[n-2]
+          if (pos_dis > LDG*1000000) {
+            assign(paste0(traitname, chr[i], "MTA_", index1), snp_chr[(n-index2):(n-1)])
+          }else{
+            assign(paste0(traitname, chr[i], "MTA_", index1), snp_chr[(n-index2-1):(n-1)])
           }
       }
     }
